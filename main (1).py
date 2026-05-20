d57db0c18a5b3ce6bdabe9ba237f0d3756d32ddf
qwertyuiopputter08-dot <qwertyuiopputter08@gmail.com>
2026-05-20 23:20:38 +0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+import time
+
+def gold_sniper_99():
+    print("Gold Sniper 99.0 Active!")
+    print("System: Fibonacci 0.382-1.882 & BIS Enabled")
+    
+    while True:
+        # บอตกำลังทำงานวิเคราะห์ราคา...
+        time.sleep(1)
+        print("Monitoring Gold Price...")
+
+if __name__ == "__main__":
+    gold_sniper_99()
